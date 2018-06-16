a6216c8b0d19cb05c3ddc8e4da03d6ae4cd1ee18
Thibault HENTGES <thibault.hentges@gmail.com>
2018-06-16 16:14:44 +0200

    e

--- import_to_mongodb.sh
+++ import_to_mongodb.sh
@@ -4,13 +4,17 @@ start=`date +%s`
 
 cd fake-all-data-small/meta/
 
-mongoimport -d big_data_project -c industries --type csv --file all_sites.csv --headerline
+if !(mongoimport -d big_data_project -c industries --type csv --file all_sites.csv --headerline); then
+    /Users/Thibault/mongodb-osx-x86_64-3.6.5/bin/mongoimport -d big_data_project -c industries --type csv --file all_sites.csv --headerline
+fi
 
 cd ../csv/
 
 for csv_file in *.csv;
 do
-    mongoimport --db big_data_project -c "$csv_file" --type csv --file "$csv_file" --headerline
+    if !(mongoimport --db big_data_project -c "$csv_file" --type csv --file "$csv_file" --headerline); then
+        /Users/Thibault/mongodb-osx-x86_64-3.6.5/bin/mongoimport --db big_data_project -c "$csv_file" --type csv --file "$csv_file" --headerline
+    fi
 done;
 
 cd ../../
